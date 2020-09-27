eb7c6a6a36788e6bf8f228ac19a4d3d6c9b488ef
Irish822 <55863255+Irish822@users.noreply.github.com>
2020-09-27 23:05:00 +0800

    Add files via upload

---
+++ resources/views/pistol/index.blade.php
@@ -0,0 +1,45 @@
+
+@extends('layouts.master')
+
+@section('content')
+<body>
+  <nav class="light-blue lighten-1" role="navigation">
+    
+        <li><a href="/home">Home</a></li> <li>
+      </ul>
+
+      <ul id="nav-mobile" class="sidenav">
+        <li><a href="#">Navbar Link</a></li>
+      </ul>
+      <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+  <div class="section no-pad-bot" id="index-banner">
+    <div class="container">
+      <br><br>
+      <center><h1>Pistols</h1></center>
+
+      <a href="/pistols/create"> Create</a>
+     
+       <ol> 
+         @foreach($pistols as $pistol)
+         <li><a href="/pistol/{{ $pistol->id}}" >{{ $pistol->model name}}</li>
+         @endforeach
+      </ol>
+       
+
+      <br><br>
+
+    </div>
+  </div>
+
+
+  <div class="container">
+    <div class="section">
+
+
+    </div>
+    <br><br>
+  </div>
+@endsection
+  
