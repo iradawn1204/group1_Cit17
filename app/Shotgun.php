43143c839f84c1795af11296562a95a54a3adc11
iradawn1204 <iradawn04@gmail.com>
2020-10-03 21:10:10 +0800

    fixed migration and models

---
+++ app/Shotgun.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+use DB;
+use Illuminate\Database\Eloquent\Model;
+
+class Shotgun extends Model
+{
+     protected $table = 'shotguns';
+}
--- app/Users.php
+++ app/Users.php
@@ -2,9 +2,39 @@
 
 namespace App;
 use DB;
-use Illuminate\Database\Eloquent\Model;
 
-class Users extends Model
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\Users as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Users extends Authenticatable
 {
-    protected $table = 'users';
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'password','birthdate', 'place'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'name_verified_at' => 'datetime',
+    ];
 }
